c3a84d3c7064e8a46f537d2d201a850b4d349131
Tushar Naik <tushar.knaik@gmail.com>
2018-08-09 00:23:11 +0530

    reduced the size of the jar

--- bonsai-core/src/test/java/com/phonepe/platform/bonsai/core/ObjectExtractor.java
+++ bonsai-core/src/test/java/com/phonepe/platform/bonsai/core/ObjectExtractor.java
@@ -1,9 +1,10 @@
 package com.phonepe.platform.bonsai.core;
 
 import com.fasterxml.jackson.core.type.TypeReference;
-import com.github.jknack.handlebars.internal.Files;
+import com.google.common.base.Preconditions;
 
-import java.io.IOException;
+import java.io.*;
+import java.nio.charset.Charset;
 
 /**
  * @author tushar.naik
@@ -13,13 +14,35 @@ public class ObjectExtractor {
 
     public <T> T getObject(String resource, Class<T> clazz) throws IOException {
         ClassLoader classLoader = getClass().getClassLoader();
-        String read = Files.read(classLoader.getResourceAsStream(resource));
+        String read = read(classLoader.getResourceAsStream(resource));
         return Mapper.MAPPER.readValue(read, clazz);
     }
 
     public <T> T getObject(String resource, TypeReference<T> typeReference) throws IOException {
         ClassLoader classLoader = getClass().getClassLoader();
-        String read = Files.read(classLoader.getResourceAsStream(resource));
+        String read = read(classLoader.getResourceAsStream(resource));
         return Mapper.MAPPER.readValue(read, typeReference);
+    }
+
+    private static String read(InputStream source) throws IOException {
+        return read(new InputStreamReader(source, Charset.forName("UTF-8")));
+    }
+
+    private static String read(Reader source) throws IOException {
+        return read(new BufferedReader(source));
+    }
+
+    private static String read(BufferedReader source) throws IOException {
+        Preconditions.checkNotNull(source, "The input is required.");
+        try {
+            int read = source.read();
+            StringBuilder script;
+            for (script = new StringBuilder(); read != -1; read = source.read()) {
+                script.append((char) read);
+            }
+            return script.toString();
+        } finally {
+            source.close();
+        }
     }
 }
