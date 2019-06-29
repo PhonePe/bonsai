0db0b077af0e54b70a77d8922453efd60fce1c35
Tushar Naik <tushar.knaik@gmail.com>
2019-06-29 18:36:10 +0530

    using string format

--- bonsai-core/src/main/java/com/phonepe/platform/bonsai/core/vital/ComponentBonsaiTreeValidator.java
+++ bonsai-core/src/main/java/com/phonepe/platform/bonsai/core/vital/ComponentBonsaiTreeValidator.java
@@ -22,8 +22,7 @@ import java.util.stream.Stream;
  * @version 1.0  23/08/18 - 1:07 PM
  */
 public final class ComponentBonsaiTreeValidator implements BonsaiTreeValidator {
-    private static final String ERROR_FIELD_STR = "field:";
-    public static final String CANNOT_BE_NULL_STR = " cannot be null";
+    private static final String ERROR_FIELD_STR = "field:%s cannot be null";
     private BonsaiProperties bonsaiProperties;
 
     public ComponentBonsaiTreeValidator(BonsaiProperties bonsaiProperties) {
@@ -111,19 +110,19 @@ public final class ComponentBonsaiTreeValidator implements BonsaiTreeValidator {
 
     private static <T> void checkNotNull(T reference, String fieldName) {
         if (reference == null) {
-            throw new BonsaiError(BonsaiErrorCode.INVALID_INPUT, ERROR_FIELD_STR + fieldName + CANNOT_BE_NULL_STR);
+            throw new BonsaiError(BonsaiErrorCode.INVALID_INPUT, String.format(ERROR_FIELD_STR, fieldName));
         }
     }
 
     private static void checkNotNullOrEmpty(String reference, String fieldName) {
         if (Strings.isNullOrEmpty(reference)) {
-            throw new BonsaiError(BonsaiErrorCode.INVALID_INPUT, ERROR_FIELD_STR + fieldName + CANNOT_BE_NULL_STR);
+            throw new BonsaiError(BonsaiErrorCode.INVALID_INPUT, String.format(ERROR_FIELD_STR, fieldName));
         }
     }
 
     private static <T> void checkNotNullOrEmpty(Collection<T> reference, String fieldName) {
         if (reference == null || reference.isEmpty()) {
-            throw new BonsaiError(BonsaiErrorCode.INVALID_INPUT, ERROR_FIELD_STR + fieldName + CANNOT_BE_NULL_STR);
+            throw new BonsaiError(BonsaiErrorCode.INVALID_INPUT, String.format(ERROR_FIELD_STR, fieldName));
         }
     }
 
