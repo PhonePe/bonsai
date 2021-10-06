9022bddd1da2ddaaaed3b943665ce6fd8e02ed56
Tushar Naik <tushar.knaik@gmail.com>
2021-10-06 11:26:46 +0530

    added a test case for null document context handling

--- bonsai-core/src/test/java/com/phonepe/platform/bonsai/core/vital/provided/VariationSelectorEngineTest.java
+++ bonsai-core/src/test/java/com/phonepe/platform/bonsai/core/vital/provided/VariationSelectorEngineTest.java
@@ -4,15 +4,21 @@ import com.google.common.collect.Maps;
 import com.phonepe.platform.bonsai.core.vital.Context;
 import com.phonepe.platform.bonsai.models.blocks.Edge;
 import com.phonepe.platform.bonsai.models.blocks.EdgeIdentifier;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Collections;
+import java.util.Optional;
 
 public class VariationSelectorEngineTest {
     @Test
     public void testWhenNoContextThenVariationSelectionReturnsFalse() {
-        new VariationSelectorEngine<>().match(new Context(null, Maps.newHashMap()),
-                                              Collections.singletonList(Edge.builder().edgeIdentifier(new EdgeIdentifier()).build()));
+        Optional<Edge> match = new VariationSelectorEngine<>()
+                .match(new Context(null, Maps.newHashMap()),
+                       Collections.singletonList(Edge.builder()
+                                                     .edgeIdentifier(new EdgeIdentifier())
+                                                     .build()));
+        Assert.assertFalse(match.isPresent());
 
     }
 }
